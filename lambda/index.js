22c3f1e8945f19c32b31223a9e5668a13e8a71c4
Arnout Cator <arnout.cator@paconsulting.com>
2020-10-29 22:15:03 +0000

    skill resumption private beta: adding Resume Session API logic to OrderIntentHandler - refactoring LaunchHandler

--- lambda/index.js
+++ lambda/index.js
@@ -20,6 +20,7 @@ const sprintf       = require('sprintf-js').sprintf;
 const _             = require('lodash');
 
 // IntentHandlers
+const LaunchHandler = require('./handlers/LaunchHandler.js');
 const OrderIntentHandler = require('./handlers/OrderIntentHandler.js');
 
 // Localization strings
@@ -28,18 +29,8 @@ const resources     = require('./resources')
 const requestUtils  = require('./requestUtils');
 // Static list of menu items with some helper functions
 const menu          = require('./menu');
-
-// APL docs
-const welcome_apl   = require('./launch_request.json');
-
-const states = {
-    PROMPTED_FOR_DAILY_SPECIALS: 'PROMPTED_FOR_DAILY_SPECIALS',
-    PROMPTED_TO_ORDER_DAILY_SPECIAL: 'PROMPTED_TO_ORDER_DAILY_SPECIAL',
-    PROMPTED_TO_CUSTOMIZE : 'PROMPTED_TO_CUSTOMIZE',
-    PROMPTED_TO_ADD_TO_ORDER: 'PROMPTED_TO_ADD_TO_ORDER',
-    PROMPTED_TO_ORDER_SPECIAL : 'PROMPTED_TO_ORDER_SPECIAL',
-    PROMPTED_TO_CUSTOMIZE_SPECIAL_MENU : 'PROMPTED_TO_CUSTOMIZE_SPECIAL_MENU'
-};
+// Static list of states
+const states        = require('/states');
 
 // Persistence
 let persistenceAdapter;
@@ -51,87 +42,6 @@ persistenceAdapter = new DynamoDbPersistenceAdapter({
     createTable: true
 });
 
-
-// *****************************************************************************
-// Launch request handler.
-// *****************************************************************************
-const LaunchHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
-    },
-    /**
-     * Launch request handler. 
-     * on launching the skill, user gets the 'speechOutput' message of this handler. 
-     * If user is silent or speaks something which is unrelated then user is reprompted with repromptOutput
-     *
-     * @param handlerInput {HandlerInput}
-     * @returns {Response}
-     */
-    async handle(handlerInput) {
-        const personId = requestUtils.getPersonId(handlerInput);
-        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-        const {in_progress} = sessionAttributes;
-        console.log('In LaunchHandler');
-        console.log('sessionAttributes: ' + JSON.stringify(sessionAttributes));
-        
-        let speakOutput, reprompt;
-        // if they had 'in flight' orders that had not been moved to ordered.
-        if(in_progress){
-            console.log('in progress orders');
-            if(personId){
-                speakOutput = handlerInput.t('WELCOME_PERSONALIZED', {
-                    personId: personId,
-                    prompt: handlerInput.t('WELCOME_BACK')
-                });
-            } else {
-                speakOutput = handlerInput.t('WELCOME_BACK');
-            }
-            reprompt = handlerInput.t('WELCOME_BACK_REPROMPT');
-            // the in-progress prompt asks them if they'd like to customize anything
-            // let's set that state for the Yes/No Intent Handlers
-            sessionAttributes.state = states.PROMPTED_TO_CUSTOMIZE;
-        } else {
-            // no in progress orders
-            console.log('no in progress orders');
-            let {day, period} = await requestUtils.getDayAndPeriod(handlerInput);
-            console.log('day ' + JSON.stringify(day));
-            console.log('period ' + JSON.stringify(period));
-            reprompt = handlerInput.t('WELCOME_REPROMPT');
-            if (personId) {
-                // Speaker is recognized, so greet by name
-                speakOutput = handlerInput.t('WELCOME_PERSONALIZED', {
-                    personId: personId,
-                    prompt: handlerInput.t('WELCOME', {
-                        day: day,
-                        period: period
-                    })
-                });
-            } else {
-                // Speaker is not recognized; give a generic greeting asking if they would like to hear our specials
-                speakOutput = handlerInput.t('WELCOME', {
-                    day: day,
-                    period: period
-                });
-            }
-            // give context to yes/no response by saving state
-            sessionAttributes.state = states.PROMPTED_FOR_DAILY_SPECIALS;
-        }
-        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']){
-            handlerInput.responseBuilder.addDirective({
-                type: 'Alexa.Presentation.APL.RenderDocument',
-                token: "welcomeToken",
-                document: welcome_apl
-            });
-        }
-        return handlerInput.responseBuilder
-                .speak(speakOutput)
-                .withSimpleCard(
-                    "Welcome", 
-                    "Welcome to My Daily Menu?")
-                .reprompt(reprompt)
-                .getResponse();
-    }
-};
 /**
  * AMAZON.YesIntentHandler. 
  * 
