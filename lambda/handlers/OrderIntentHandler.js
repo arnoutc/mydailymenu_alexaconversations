eb8c9c52a44c668173d8ea63c58a9656abd6db0c
Arnout Cator <arnout.cator@paconsulting.com>
2020-10-29 21:45:44 +0000

    skill resumption private beta: adding Resume Session API logic to OrderIntentHandler - refactoring OrderIntentHandler in separate module with js extension

---
+++ lambda/handlers/OrderIntentHandler.js
@@ -0,0 +1,43 @@
+const Alexa = require('ask-sdk-core');
+
+/**
+ * Adding skill resumption to put the order in background.
+ */
+const OrderIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
+                && Alexa.getIntentName(handlerInput.requestEnvelope) === 'OrderIntent';
+    },
+    handle(handlerInput) {
+        console.log("In OrderIntentHandler");
+
+        /**
+         * Token exchange 
+         */
+        const userId = handlerInput.requestEnvelope.context.System.user.userId;
+        console.log(`user id is ${userId}`);
+       
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        _.defaults(sessionAttributes, {
+            orders: []
+        });
+        const {in_progress} = sessionAttributes;
+        let orderText = menu.generateOrderText(in_progress);
+        sessionAttributes.orders.push({ 
+            date : new Date().toISOString(),
+            order: in_progress
+        });  
+        let speakOutput = handlerInput.t('PLACE_ORDER', {
+            orderText : orderText
+        });
+        // let reprompt = handlerInput.t('PLACE_ORDER_REPROMPT');
+    
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .withSessionBehavior("BACKGROUNDED")
+            // .reprompt(reprompt)
+            .getResponse();
+    }
+}
+
+module.exports = OrderIntentHandler;
