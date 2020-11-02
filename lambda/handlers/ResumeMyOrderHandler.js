700b4d2844648acb45d6ee2bd51f1a93c13752f7
Arnout Cator <arnout.cator@paconsulting.com>
2020-11-02 23:15:49 +0000

    skill resumption private beta: adding Resume Session API logic - adding ResumeMyOrderHandler.js import es6 for async

--- lambda/handlers/ResumeMyOrderHandler.js
+++ lambda/handlers/ResumeMyOrderHandler.js
@@ -1,6 +1,5 @@
-
 const Alexa = require('ask-sdk-core');
-let AuthTokenHandler = await import('./AuthTokenHandler.js');
+const AuthTokenHandler =  require('./AuthTokenHandler.js');
 
 const ResumeMyOrderHandler = {
     
@@ -8,16 +7,17 @@ const ResumeMyOrderHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ResumeMyOrderHandler';
       },
-      handle(handlerInput) {
+      async handle(handlerInput) {
         console.log(JSON.stringify(handlerInput.requestEnvelope, 0, null));
     
-        const apiAccessToken =  AuthTokenHandler.getToken(Alexa.getUserId(handlerInput.requestEnvelope));
+        const apiAccessToken = await AuthTokenHandler.getToken(Alexa.getUserId(handlerInput.requestEnvelope))
 
         if (apiAccessToken) {
         console.log(`Found apiAccessToken ${apiAccessToken}, scheduling a resumption`);
-        scheduleResumption(stage.value.toLowerCase(), region.value.toLowerCase(), sessionId, apiAccessToken, delay.value)
-            .then((data) => console.log(`MessageID is ${data.MessageId}`))
-            .catch((err) => console.error(err, err.stack));
+        
+        // scheduleResumption(stage.value.toLowerCase(), region.value.toLowerCase(), sessionId, apiAccessToken, delay.value)
+        //     .then((data) => console.log(`MessageID is ${data.MessageId}`))
+        //     .catch((err) => console.error(err, err.stack));
         }
 
         return handlerInput.responseBuilder.getResponse();
