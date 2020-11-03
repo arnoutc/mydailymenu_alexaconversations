7430cde3c5819dcd6b78be090277aa758c0f4626
Arnout Cator <arnout.cator@paconsulting.com>
2020-11-03 22:15:20 +0000

    skill resumption private beta: adding Resume Session API logic - adding AuthTokenHandler handle to logic debugging

--- lambda/handlers/OrderIntentHandler.js
+++ lambda/handlers/OrderIntentHandler.js
@@ -53,16 +53,15 @@ const OrderIntentHandler = {
             //redirect the user to the Alexa app to grant permission
             console.log(`initiate token exchange and persist token in Dynamodb table of request envelope: ${JSON.stringify(handlerInput.requestEnvelope)} from user ${Alexa.getUserId(handlerInput.requestEnvelope)}`);
 
-            // const token =  await AuthTokenHandler.handle(handlerInput.requestEnvelope);
-            // console.log(`handle --- token to return is ${JSON.stringify(token)}`);
-            // return token;
+            const token =  await AuthTokenHandler.handle(handlerInput.requestEnvelope);
+            console.log(`handle --- token to return is ${JSON.stringify(token)}`);
+        
             var speechText = "You must have a My Daily Menu account to order a menu. " +
             "Please use the Alexa app to link your Amazon account " +
             "with your My Daily Menu Account.";
 
             return handlerInput.responseBuilder
                 .speak(speechText)
-                .withLinkAccountCard()
                 .getResponse();
         }
     }
