341d8b76efb1ab063e7b5608f1b61fc9f3119383
Arnout Cator <arnout.cator@paconsulting.com>
2020-11-03 22:40:10 +0000

    skill resumption private beta: adding Resume Session API logic - adding AuthTokenHandler handle to logic debugging

--- lambda/handlers/AuthorizationGrantHandler.js
+++ lambda/handlers/AuthorizationGrantHandler.js
@@ -7,7 +7,7 @@ const AuthorizationGrantHandler = {
     },
   
     handle(handlerInput) {
-      console.log(`Received an ${Alexa.RequestTypes.ALEXA_AUTHORIZATION_GRANT_REQUEST}`);
+      //console.log(`Received an ${Alexa.RequestTypes.ALEXA_AUTHORIZATION_GRANT_REQUEST}`);
       console.log(JSON.stringify(handlerInput.requestEnvelope, 0, null));
       AuthTokenHandler.handle(handlerInput.requestEnvelope);
   
