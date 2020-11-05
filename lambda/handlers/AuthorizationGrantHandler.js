786cddcaa52d6a65dfd3c87eb671744dabb19c94
Arnout Cator <arnout.cator@paconsulting.com>
2020-11-05 09:11:44 +0000

    skill resumption private beta: adding Resume Session API logic - refactored ALEXA_AUTHORIZATION_GRANT_REQUEST

--- lambda/handlers/AuthorizationGrantHandler.js
+++ lambda/handlers/AuthorizationGrantHandler.js
@@ -1,17 +1,16 @@
 const Alexa = require('ask-sdk-core');
 const AuthTokenHandler = require('./AuthTokenHandler.js');
+const ALEXA_AUTHORIZATION_GRANT_REQUEST = 'Alexa.Authorization.Grant'
 
 const AuthorizationGrantHandler = {
     canHandle(handlerInput) {
       console.log(`AuthorizationGrantHandler --- canHandle -- requestEnvelope is ${JSON.stringify(handlerInput.requestEnvelope)}`);
-      console.log(`AuthorizationGrantHandler --- canHandle -- requestTypes are ${JSON.stringify(Alexa.RequestTypes)}`);
 
-      return Alexa.getRequestType(handlerInput.requestEnvelope) === Alexa.RequestTypes.ALEXA_AUTHORIZATION_GRANT_REQUEST;
+      return Alexa.getRequestType(handlerInput.requestEnvelope) === ALEXA_AUTHORIZATION_GRANT_REQUEST;
     },
   
     handle(handlerInput) {
-      console.log(`AuthorizationGrantHandler --- handle -- requestEnvelope is ${JSON.stringify(handlerInput.requestEnvelope)}`);
-      console.log(`AuthorizationGrantHandler --- handle -- Received an ${Alexa.RequestTypes.ALEXA_AUTHORIZATION_GRANT_REQUEST}`);
+      console.log(`AuthorizationGrantHandler --- handle -- Received an ${ALEXA_AUTHORIZATION_GRANT_REQUEST}`);
       console.log(JSON.stringify(handlerInput.requestEnvelope, 0, null));
       AuthTokenHandler.handle(handlerInput.requestEnvelope);
   
