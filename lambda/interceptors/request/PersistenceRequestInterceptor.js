7bf9b46f7e14cec736df1edef78ef0a99d03019f
Arnout Cator <arnout.cator@paconsulting.com>
2020-11-05 08:59:34 +0000

    skill resumption private beta: adding Resume Session API logic - refactor session check using RequestEnvelopeUtils Alexa.isNewSession

--- lambda/interceptors/request/PersistenceRequestInterceptor.js
+++ lambda/interceptors/request/PersistenceRequestInterceptor.js
@@ -1,32 +1,24 @@
-const Alexa = require('ask-sdk-core');
-
 /** 
  * This request interceptor with each new session loads all global persistent attributes
  * into the session attributes and increments a launch counter 
 */
 const PersistenceRequestInterceptor = { 
     process(handlerInput) { 
-        console.log(`PersistenceRequestInterceptor  -- start `);
-        if(Alexa.isNewSession(handlerInput.requestEnvelope )) { 
-            console.log(`PersistenceRequestInterceptor  -- new session`);
-                return new Promise((resolve, reject) => { 
-                    handlerInput.attributesManager.getPersistentAttributes() 
-                        .then((persistentAttributes) => { 
-                            persistentAttributes = persistentAttributes || {};
-                            if(!persistentAttributes['launchCount'])
-                              persistentAttributes['launchCount'] = 0;
-                            persistentAttributes['launchCount'] += 1; 
-                            console.log(`PersistenceRequestInterceptor  -- launchCount incremented`);
-                            handlerInput.attributesManager.setSessionAttributes(persistentAttributes); 
-                            console.log(`PersistenceRequestInterceptor  -- persistentAttributes ${JSON.stringify(persistentAttributes)}`);
-                            resolve();
-                        })
-                        .catch((err) => { 
-                          reject(err); 
-                      });
-                }); 
-            
-        
+        if(handlerInput.requestEnvelope.session['new']) { 
+            return new Promise((resolve, reject) => { 
+                handlerInput.attributesManager.getPersistentAttributes() 
+                    .then((persistentAttributes) => { 
+                        persistentAttributes = persistentAttributes || {};
+                        if(!persistentAttributes['launchCount'])
+                          persistentAttributes['launchCount'] = 0;
+                        persistentAttributes['launchCount'] += 1; 
+                        handlerInput.attributesManager.setSessionAttributes(persistentAttributes); 
+                        resolve();
+                    })
+                    .catch((err) => { 
+                      reject(err); 
+                  });
+            }); 
         } // end session['new'] 
     } 
   }
