51f17f0bd464987541fae987ce36bc1813438bd0
Arnout Cator <arnout.cator@paconsulting.com>
2020-11-15 20:20:33 +0000

    skill resumption private beta: adding Resume Session API logic - OrderIntentHandler not working - get existing user check in docclient

--- lambda/handlers/AuthTokenHandler.js
+++ lambda/handlers/AuthTokenHandler.js
@@ -54,7 +54,7 @@ const storeCredentials = async (userId, accessToken, refreshToken, expiresIn, la
 
   const params = {
     TableName: TABLE_NAME,
-    Key: userId,
+    Item: item
   };
 
   //add also to an initial session, if there is no existing session for the user
