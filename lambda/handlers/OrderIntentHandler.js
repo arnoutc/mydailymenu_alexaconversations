56b40aded2ebe3b47d3913e2ee39978d15baff06
Arnout Cator <arnout.cator@paconsulting.com>
2020-11-15 11:17:57 +0000

    skill resumption private beta: adding Resume Session API logic - OrderIntentHandler not working - get existing user item check

--- lambda/handlers/OrderIntentHandler.js
+++ lambda/handlers/OrderIntentHandler.js
@@ -1,65 +1,94 @@
-const Alexa = require('ask-sdk-core');
+//const Alexa = require('ask-sdk-core');
 const _             = require('lodash');
 const menu          = require('../menu.js');
-const AuthTokenHandler = require('./AuthTokenHandler.js');
+//const AuthTokenHandler = require('./AuthTokenHandler.js');
 
 //const {scheduleResumption } = require('./ResumeMyOrderHandler.js');
 
-const OrderIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'OrderIntent';
-    },
-    async handle(handlerInput) {
-        console.log("In OrderIntentHandler");
+// const OrderIntentHandler = {
+//     canHandle(handlerInput) {
+//         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
+//             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'OrderIntent';
+//     },
+//     async handle(handlerInput) {
+//         console.log("In OrderIntentHandler");
 
-        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-        _.defaults(sessionAttributes, {
-            orders: []
-        });
-        const {in_progress} = sessionAttributes;
-        let orderText = menu.generateOrderText(in_progress);
-        sessionAttributes.orders.push({ 
-            date : new Date().toISOString(),
-            order: in_progress
-        });  
-        let speakOutput = handlerInput.t('PLACE_ORDER', {
-            orderText : orderText
-        });
+//         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+//         _.defaults(sessionAttributes, {
+//             orders: []
+//         });
+//         const {in_progress} = sessionAttributes;
+//         let orderText = menu.generateOrderText(in_progress);
+//         sessionAttributes.orders.push({ 
+//             date : new Date().toISOString(),
+//             order: in_progress
+//         });  
+//         let speakOutput = handlerInput.t('PLACE_ORDER', {
+//             orderText : orderText
+//         });
 
-        //let reprompt = handlerInput.t('PLACE_ORDER_REPROMPT');
+//         //let reprompt = handlerInput.t('PLACE_ORDER_REPROMPT');
 
-        /* 
-            Check if the user has granted permissions for the skill to read and write reminders.
-            If the permissions has not been granted, send an AskForPermissionsConsent card to the Alexa Companion mobile app.
-            Reference: https://developer.amazon.com/docs/custom-skills/request-customer-contact-information-for-use-in-your-skill.html#permissions-card-for-requesting-customer-consent
-        */
-       //const apiAccessToken = await AuthTokenHandler.getToken(Alexa.getUserId(handlerInput.requestEnvelope));
-       //console.log(`OrderIntentHandler --- apiAccessToken is ${JSON.stringify(apiAccessToken)}`);
+//         /* 
+//             Check if the user has granted permissions for the skill to read and write reminders.
+//             If the permissions has not been granted, send an AskForPermissionsConsent card to the Alexa Companion mobile app.
+//             Reference: https://developer.amazon.com/docs/custom-skills/request-customer-contact-information-for-use-in-your-skill.html#permissions-card-for-requesting-customer-consent
+//         */
+//        //const apiAccessToken = await AuthTokenHandler.getToken(Alexa.getUserId(handlerInput.requestEnvelope));
+//        //console.log(`OrderIntentHandler --- apiAccessToken is ${JSON.stringify(apiAccessToken)}`);
 
-    //    if (apiAccessToken) {
-    //      console.log(`Found apiAccessToken ${apiAccessToken}, scheduling a resumption`);
-    //      scheduleResumption(stage.value.toLowerCase(), region.value.toLowerCase(), sessionId, apiAccessToken, delay.value)
-    //        .then((data) => console.log(`MessageID is ${data.MessageId}`))
-    //        .catch((err) => console.error(err, err.stack));
-    //    }
+//     //    if (apiAccessToken) {
+//     //      console.log(`Found apiAccessToken ${apiAccessToken}, scheduling a resumption`);
+//     //      scheduleResumption(stage.value.toLowerCase(), region.value.toLowerCase(), sessionId, apiAccessToken, delay.value)
+//     //        .then((data) => console.log(`MessageID is ${data.MessageId}`))
+//     //        .catch((err) => console.error(err, err.stack));
+//     //    }
 
-        // if(apiAccessToken){ //if Skill Resumption is enabled, use backgrounding
-        //     return handlerInput.responseBuilder
-        //     .speak(speakOutput)
-        //     .withSessionBehavior("BACKGROUNDED")
-        //     .getResponse();
-        // } else {
-        //     return handlerInput.responseBuilder
-        //         .speak(speakOutput)
-        //         //.withAskForPermissionsConsentCard(['alexa::skill:resumption'])
-        //         .getResponse()
-        // } 
+//         // if(apiAccessToken){ //if Skill Resumption is enabled, use backgrounding
+//         //     return handlerInput.responseBuilder
+//         //     .speak(speakOutput)
+//         //     .withSessionBehavior("BACKGROUNDED")
+//         //     .getResponse();
+//         // } else {
+//         //     return handlerInput.responseBuilder
+//         //         .speak(speakOutput)
+//         //         //.withAskForPermissionsConsentCard(['alexa::skill:resumption'])
+//         //         .getResponse()
+//         // } 
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .getResponse()
+//         return handlerInput.responseBuilder
+//             .speak(speakOutput)
+//             .getResponse()
+//     }
+//}
+
+    const OrderIntentHandler = {
+        canHandle(handlerInput) {
+            const request = handlerInput.requestEnvelope.request;
+            return request.type === 'IntentRequest' && request.intent.name === 'OrderIntent';
+        },
+        handle(handlerInput) {
+            console.log("In OrderIntentHandler");
+            const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+            _.defaults(sessionAttributes, {
+                orders: []
+            });
+            const {in_progress} = sessionAttributes;
+            let orderText = menu.generateOrderText(in_progress);
+            sessionAttributes.orders.push({ 
+                date : new Date().toISOString(),
+                order: in_progress
+            });  
+            let speakOutput = handlerInput.t('PLACE_ORDER', {
+                orderText : orderText
+            });
+            let reprompt = handlerInput.t('PLACE_ORDER_REPROMPT');
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt(reprompt)
+                .getResponse();
+        }
     }
-}
+
 
 module.exports = OrderIntentHandler;
